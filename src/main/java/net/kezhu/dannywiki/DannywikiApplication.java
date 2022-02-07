f878c1c9eb5cb86df77ed4a2e3fb4e29a3059542
DanielTeng <419885411@qq.com>
2022-02-07 23:06:10 +0800

    2-5 修改启动方案

--- src/main/java/net/kezhu/dannywiki/DannywikiApplication.java
+++ src/main/java/net/kezhu/dannywiki/DannywikiApplication.java
@@ -1,17 +1,20 @@
 package net.kezhu.dannywiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DannywikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DannywikiApplication.class);
+
     public static void main(String[] args) {
-
-        SpringApplication.run(DannywikiApplication.class, args);
-        System.out.println("xxx");
+        SpringApplication app = new SpringApplication(DannywikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
-
 }
