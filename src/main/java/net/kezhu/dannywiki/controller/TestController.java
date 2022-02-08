9b270c3abc5123bfd5f2c5ebf073b5ff5776fd36
DanielTeng <419885411@qq.com>
2022-02-08 16:44:30 +0800

    2-8 定义和使用自定义配置项

--- src/main/java/net/kezhu/dannywiki/controller/TestController.java
+++ src/main/java/net/kezhu/dannywiki/controller/TestController.java
@@ -1,16 +1,18 @@
 package net.kezhu.dannywiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class TestController {
+    @Value("${test.hello}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){
