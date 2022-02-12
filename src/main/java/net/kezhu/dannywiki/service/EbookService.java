a45c31fc39030b24e53106b6c09855760346ab24
DanielTeng <419885411@qq.com>
2022-02-12 11:50:13 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/net/kezhu/dannywiki/service/EbookService.java
+++ src/main/java/net/kezhu/dannywiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package net.kezhu.dannywiki.service;
 
 import net.kezhu.dannywiki.domain.Ebook;
+import net.kezhu.dannywiki.domain.EbookExample;
 import net.kezhu.dannywiki.mapper.EbookMapper;
+import net.kezhu.dannywiki.req.EbookReq;
+import net.kezhu.dannywiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +17,18 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list () {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list (EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+//            ebookResp.setId(ebook.getId());
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
