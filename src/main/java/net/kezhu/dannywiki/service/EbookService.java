a1238efd9a36225e6866ee2ce5ff3219d6b03fa7
DanielTeng <419885411@qq.com>
2022-02-12 23:44:51 +0800

    3-8 新增CopyUtil封装BeanUtils 适用List的copy

--- src/main/java/net/kezhu/dannywiki/service/EbookService.java
+++ src/main/java/net/kezhu/dannywiki/service/EbookService.java
@@ -5,11 +5,10 @@ import net.kezhu.dannywiki.domain.EbookExample;
 import net.kezhu.dannywiki.mapper.EbookMapper;
 import net.kezhu.dannywiki.req.EbookReq;
 import net.kezhu.dannywiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import net.kezhu.dannywiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,13 +21,14 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-//            ebookResp.setId(ebook.getId());
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+////            ebookResp.setId(ebook.getId());
+//            BeanUtils.copyProperties(ebook, ebookResp);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
         return respList;
     }
 }
